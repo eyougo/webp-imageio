7c265949ba494cfb6640871cf7e444ce54eb6498
eyougo <meihongyuan@163.com>
2015-09-08 16:10:56 +0800

    add support for VP8L: WebP Lossless Bitstream Specification

--- src/javase/java/com/luciad/imageio/webp/WebPImageWriterSpi.java
+++ src/javase/java/com/luciad/imageio/webp/WebPImageWriterSpi.java
@@ -34,77 +34,76 @@ import java.util.Locale;
  *
  */
 public class WebPImageWriterSpi extends ImageWriterSpi {
-  public WebPImageWriterSpi() {
-    super(
-        "Luciad",
-        "1.0",
-        new String[]{ "WebP", "webp" },
-        new String[]{ "webp" },
-        new String[]{ "image/webp" },
-        WebPReader.class.getName(),
-        new Class[]{ ImageOutputStream.class },
-        new String[]{ WebPImageReaderSpi.class.getName() },
-        false,
-        null,
-        null,
-        null,
-        null,
-        false,
-        null,
-        null,
-        null,
-        null
-    );
-  }
-
-  @Override
-  public boolean canEncodeImage( ImageTypeSpecifier type ) {
-    ColorModel colorModel = type.getColorModel();
-    SampleModel sampleModel = type.getSampleModel();
-    int transferType = sampleModel.getTransferType();
-
-    if ( colorModel instanceof ComponentColorModel ) {
-      if ( !( sampleModel instanceof ComponentSampleModel ) ) {
-        return false;
-      }
-
-      if ( transferType != DataBuffer.TYPE_BYTE && transferType != DataBuffer.TYPE_INT ) {
-        return false;
-      }
-    }
-    else if ( colorModel instanceof DirectColorModel ) {
-      if ( !( sampleModel instanceof SinglePixelPackedSampleModel ) ) {
-        return false;
-      }
-
-      if ( transferType != DataBuffer.TYPE_INT ) {
-        return false;
-      }
+    public WebPImageWriterSpi() {
+        super(
+                "Luciad",
+                "1.0",
+                new String[]{"WebP", "webp"},
+                new String[]{"webp"},
+                new String[]{"image/webp"},
+                WebPReader.class.getName(),
+                new Class[]{ImageOutputStream.class},
+                new String[]{WebPImageReaderSpi.class.getName()},
+                false,
+                null,
+                null,
+                null,
+                null,
+                false,
+                null,
+                null,
+                null,
+                null
+        );
     }
 
-    ColorSpace colorSpace = colorModel.getColorSpace();
-    if ( !( colorSpace.isCS_sRGB() ) ) {
-      return false;
+    @Override
+    public boolean canEncodeImage(ImageTypeSpecifier type) {
+        ColorModel colorModel = type.getColorModel();
+        SampleModel sampleModel = type.getSampleModel();
+        int transferType = sampleModel.getTransferType();
+
+        if (colorModel instanceof ComponentColorModel) {
+            if (!(sampleModel instanceof ComponentSampleModel)) {
+                return false;
+            }
+
+            if (transferType != DataBuffer.TYPE_BYTE && transferType != DataBuffer.TYPE_INT) {
+                return false;
+            }
+        } else if (colorModel instanceof DirectColorModel) {
+            if (!(sampleModel instanceof SinglePixelPackedSampleModel)) {
+                return false;
+            }
+
+            if (transferType != DataBuffer.TYPE_INT) {
+                return false;
+            }
+        }
+
+        ColorSpace colorSpace = colorModel.getColorSpace();
+        if (!(colorSpace.isCS_sRGB())) {
+            return false;
+        }
+
+        int[] sampleSize = sampleModel.getSampleSize();
+        for (int i = 0; i < sampleSize.length; i++) {
+            if (sampleSize[i] > 8) {
+                return false;
+            }
+        }
+
+
+        return true;
     }
 
-    int[] sampleSize = sampleModel.getSampleSize();
-    for ( int i = 0; i < sampleSize.length; i++ ) {
-      if ( sampleSize[ i ] > 8 ) {
-        return false;
-      }
+    @Override
+    public ImageWriter createWriterInstance(Object extension) throws IOException {
+        return new WebPWriter(this);
     }
 
-
-    return true;
-  }
-
-  @Override
-  public ImageWriter createWriterInstance( Object extension ) throws IOException {
-    return new WebPWriter( this );
-  }
-
-  @Override
-  public String getDescription( Locale locale ) {
-    return "WebP Writer";
-  }
+    @Override
+    public String getDescription(Locale locale) {
+        return "WebP Writer";
+    }
 }
